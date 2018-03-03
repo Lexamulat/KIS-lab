a4b84977203ae5289f4e8f87487b64be5dc70c09
Grishy <grishy.ru@yandex.ru>
2018-03-03 23:22:05 +0300

    modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("JS")
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+    //initialize all modals           
+    $('.modal').modal();
+
+}, false);
+
+function edit(id) {
+    console.log(id)
+}
+
+function del(id) {
+    console.log(id)
+}

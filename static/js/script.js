2dddd8eacaae0f72c6c7c10f9a44dd67ee8bfc38
lexamulat <lexamulat@mail.ru>
2018-03-07 23:33:29 +0300

    select work

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,23 @@
 "use strict";
 
-$(document).ready(function() {
-   
+$(document).ready(function () {
+
     console.log("start")
     updateList()
     console.log("end")
-    var options =  {
+    var options = {
         content: "added23", // text of the snackbar
         style: "toast", // add a custom class to your snackbar
         timeout: 2000 // time in milliseconds after the snackbar autohides, 0 is disabled
     }
-    
-    
+
+
     // $.snackbar(options);
 
 
-    $("#CATADD").on('click', function() {
-    // var namecat=$("#namecat").val();
-    // var urlcat=$("#urlcat").val();
+    $("#CATADD").on('click', function () {
+        // var namecat=$("#namecat").val();
+        // var urlcat=$("#urlcat").val();
 
         let out = {
             name_cat: $("#name_cat").val(),
@@ -27,28 +27,28 @@ $(document).ready(function() {
         document.getElementById("url_cat").value = "";
 
 
-        $.post("insert", JSON.stringify(out), function(data, textStatus) {
+        $.post("insert", JSON.stringify(out), function (data, textStatus) {
 
-            if((data)==0){
-                
+            if ((data) == 0) {
+
                 alert("failed");
 
                 // $("#snak").snackbar("show");
 
-                }else{
-                    console.log(options)
-                    $.snackbar(options)
-                 //alert("succsess") 
+            } else {
+                console.log(options)
+                $.snackbar(options)
+                //alert("succsess") 
                 //  $("#snak").snackbar("show");
-                 
-                }
 
-           
+            }
+
+
         }, "json");
     });
 
 
-    $("#two").on('click', function() {
+    $("#two").on('click', function () {
 
         $.snackbar(options)
 
@@ -56,34 +56,49 @@ $(document).ready(function() {
 
 
 
+    var CategoryActivePoint = 0
 
 
     function updateList() {
-        
-        $.post("list",function(data, textStatus) {
+
+        $.post("list", function (data, textStatus) {
             let list = $("#listCategory")
-              list.empty()
-              
-   var ActiveFlag=true;
-    $.each(data, function(key, val) {
-        console.log(val)
-        if(ActiveFlag==true){
-          var  flg="active";
-        }else{
-          var  flg=""
-        }
-        let el = `
-        <div class="list-group-item list-group-item-action ${flg}">${val.name}
-        </div>
-        `
-            list.append(el)
-            ActiveFlag=false
+            list.empty()
+
+            var ActiveFlag = true;
+            $.each(data, function (key, val) {
+                console.log(val)
+                if (ActiveFlag == true) {
+                    var flg = "active";
+                } else {
+                    var flg = ""
+                }
+                let el = `
+                    <div class="list-group-item list-group-item-action ${flg} ClassCategory" data-CatId=${val.id}>${val.name}
+                    </div>
+                    `
+                list.append(el)
+
+
+                ActiveFlag = false
             });
+
+            $(".list-group-item").click((t) => {
+        
+                console.log($(t.currentTarget).attr("data-Catid"))
+
+                $(".ClassCategory").removeClass('active')
+
+                
+                console.log($(t.currentTarget).addClass("active"))
+            })
+         
+        
+        
+        
 
         }, "json");
     }
 
-
-    
 });
 

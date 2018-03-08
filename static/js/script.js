dd7860f91e191c07e8b8c31e249eafc499998f09
Grishy <grishy.ru@yandex.ru>
2018-03-08 15:53:07 +0300

    Ser`ga make K P A C U B O

--- static/js/script.js
+++ static/js/script.js
@@ -1,171 +1,128 @@
 "use strict";
 
-$(document).ready(function() {
+const LAB = {
+    toast: function(text) {
+        $.snackbar({
+            content: text,
+            style: "toast",
+            timeout: 2000
+        })
+    },
+}
 
-    console.log("start")
-    updateList()
-    console.log("end")
-    var options = {
-        content: "added23", // text of the snackbar
-        style: "toast", // add a custom class to your snackbar
-        timeout: 2000 // time in milliseconds after the snackbar autohides, 0 is disabled
-    }
-
-
-    // $.snackbar(options);
-
-
-    $("#CATADD").on('click', function() {
-        // var namecat=$("#namecat").val();
-        // var urlcat=$("#urlcat").val();
-
-        let out = {
-            name_cat: $("#name_cat").val(),
-            url_cat: $("#url_cat").val()
-        }
-        document.getElementById("name_cat").value = "";
-        document.getElementById("url_cat").value = "";
-
-
-        $.post("insert", JSON.stringify(out), function(data, textStatus) {
-
-            if ((data) == 0) {
-
-                alert("failed");
-
-                // $("#snak").snackbar("show");
-
-            } else {
-                console.log(options)
-                $.snackbar(options)
-                    //alert("succsess") 
-                    //  $("#snak").snackbar("show");
-
-            }
-
-
+function labPost(url, postData) {
+    return new Promise(resolve => {
+        $.post(url, postData, function(data, textStatus) {
+            resolve(data)
         }, "json");
     });
+}
 
+async function update() {
+    const dataCat = await labPost("list")
 
-    $("#two").on('click', function() {
+    let list = $("#listCategory")
+    list.empty()
 
-        $.snackbar(options)
+    for (let i = 0; i < dataCat.length; i++) {
+        const el = dataCat[i];
 
-    });
-
-
-
-    var CategoryActivePoint = 0
-
-
-    function updateList() {
-
-        $.post("list", function(data, textStatus) {
-            let list = $("#listCategory")
-            list.empty()
-
-            var ActiveFlag = true;
-            $.each(data, function(key, val) {
-                console.log(val)
-                if (ActiveFlag == true) {
-                    var flg = "active";
-                } else {
-                    var flg = ""
-                }
-                let el = `
-                    <div class="list-group-item list-group-item-action ${flg} ClassCategory" data-CatId=${val.id}>${val.name}
-                    <button type="button" class="btn btn-danger btn-sm l-button_action CategoryDelete">
+        let listEl = `
+            <div class="list-group-item list-group-item-action l-cat-elem" data-cat_id=${el.id}>
+                ${el.name}
+                <button type="button" class="btn btn-danger btn-sm l-button_action CategoryDelete">
                     <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-bin"></use></svg>
-                    </button>  
-                    <button type="button" class="btn btn-success btn-sm l-button_action CategoryEdit" data-toggle="modal" data-target="#l-EditCategory" data-whatevername=${val.name} data-whateverurl=${val.url}>
+                </button>  
+                <button type="button" class="btn btn-success btn-sm l-button_action CategoryEdit" data-toggle="modal" data-target="#l-EditCategory" data-modal_name=${el.name} data-modal_url=${el.url}>
                     <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-pencil"></use></svg>
-                   </button>
-                    </div>
-                    `
+                </button>
+            </div>`
 
-                // <button type="button" class="btn btn-success addbtncat" data-toggle="modal" data-target="#l-createCategory" data-whatever="addCat">+</button>
-
-
-
-                list.append(el)
-
-
-                ActiveFlag = false
-            })
-
-            $(".ClassCategory").click((t) => {
-
-                // console.log($(t.currentTarget).attr("data-Catid"))
-
-                $(".ClassCategory").removeClass('active')
-
-
-                $(t.currentTarget).addClass("active")
-            })
-
-            $(".CategoryEdit").click((t) => {
-                console.log($(t.currentTarget).parent().attr("data-Catid"))
-
-                // console.log($(t.currentTarget).parent())
-
-
-                $('#l-EditCategory').on('show.bs.modal', function(event) {
-                    var button = $(event.relatedTarget) // Button that triggered the modal
-                    var EditingName = button.data('whatevername') // Extract info from data-* attributes
-                    var EditingUrl = button.data('whateverurl')
-                        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-                        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-                    var modal = $(this)
-
-                    modal.find('#CatEditName').val(EditingName)
-                    modal.find('#CatEditUrl').val(EditingUrl)
-                })
-
-
-
-            })
-
-
-
-        }, "json");
+        list.append(listEl)
     }
 
+    list.children().first().addClass("active")
+}
+
+async function CATADD() {
+    const dataCat = await labPost("list")
+
+    let out = {
+        name_cat: $("#name_cat").val(),
+        url_cat: $("#url_cat").val()
+    }
+
+    document.getElementById("name_cat").value = "";
+    document.getElementById("url_cat").value = "";
+
+    let res = await labPost("insert", JSON.stringify(out))
+    if (res) {
+        LAB.toast("Успешно")
+    } else {
+        LAB.toast("Ошибка доабвления")
+    }
+}
+
+async function CATADD() {
+    const dataCat = await labPost("list")
+
+    let out = {
+        name_cat: $("#name_cat").val(),
+        url_cat: $("#url_cat").val()
+    }
+
+    document.getElementById("name_cat").value = "";
+    document.getElementById("url_cat").value = "";
+
+    let res = await labPost("insert", JSON.stringify(out))
+    if (res) {
+        LAB.toast("Успешно")
+    } else {
+        LAB.toast("Ошибка доабвления")
+    }
+}
+
+async function CATEDIT() {
+    let out = {
+        name_cat: $("#CatEditName").val(),
+        url_cat: $("#CatEditUrl").val()
+    }
+
+    let res = await labPost("insert", JSON.stringify(out))
+    if (res) {
+        LAB.toast("Успешно")
+    } else {
+        LAB.toast("Ошибка редактирвоания")
+    }
+
+    update()
+}
 
 
 
+async function labStart() {
+    await update()
 
-    $("#CATEDIT").on('click', function() {
-        // var namecat=$("#namecat").val();
-        // var urlcat=$("#urlcat").val();
+    // EDIT modals
+    $("#CATADD").click(CATADD)
+    $("#CATEDIT").click(CATEDIT)
 
-        let out = {
-                name_cat: $("#CatEditName").val(),
-                url_cat: $("#CatEditUrl").val()
-            }
-            // document.getElementById("name_cat").value = "";
-            // document.getElementById("url_cat").value = "";
+    // EDIT placeholder
+    $('#l-EditCategory').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget)
+        var EditingName = button.data('modal_name')
+        var EditingUrl = button.data('modal_url')
 
-
-        $.post("insert", JSON.stringify(out), function(data, textStatus) {
-
-            if ((data) == 0) {
-
-                alert("failed");
-
-                // $("#snak").snackbar("show");
-
-            } else {
-                console.log(options)
-                $.snackbar(options)
-                    //alert("succsess") 
-                    //  $("#snak").snackbar("show");
-
-            }
-            updateList()
-
-        }, "json");
-    });
+        $(this).find('#CatEditName').val(EditingName)
+        $(this).find('#CatEditUrl').val(EditingUrl)
+    })
 
 
-});
+    // Move active
+    $(".l-cat-elem").click((t) => {
+        $(".l-cat-elem").removeClass('active')
+        $(t.currentTarget).addClass("active")
+    })
+}
+
+$(document).ready(labStart)
